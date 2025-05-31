ef893d8f3aaeb8282fa32bc9c89632813855db1b
lauramesquitabruel <lauramesquitabruel@gmail.com>
2025-05-31 19:21:23 -0300

    entendo a lógica do trabalho

--- script.sql
+++ script.sql
@@ -2,24 +2,20 @@ CREATE DATABASE bd2;
 \c bd2;
 
 CREATE UNLOGGED TABLE memoria (id SERIAL PRIMARY KEY, num INT);
-CREATE TABLE log (operacao TEXT, id_tabela_memoria INT, num INT, id_transacao INT);
+CREATE TABLE log (operacao TEXT, id_tabela_memoria INT, num INT);
 
 CREATE OR REPLACE FUNCTION atualiza_log()
 RETURNS trigger AS $$
-DECLARE
-    id_transacao_atual int;
 BEGIN
-    id_transacao_atual := txid_current();
-
     IF TG_OP = 'INSERT' THEN
-        INSERT INTO log (operacao, id_tabela_memoria, num, id_transacao)
-        VALUES ('INSERT', NEW.id, NEW.num, id_transacao_atual);
+        INSERT INTO log (operacao, id_tabela_memoria, num)
+        VALUES ('INSERT', NEW.id, NEW.num);
     ELSIF TG_OP = 'UPDATE' THEN
-        INSERT INTO log (operacao, id_tabela_memoria, num, id_transacao)
-        VALUES ('UPDATE', NEW.id, NEW.num, id_transacao_atual);
+        INSERT INTO log (operacao, id_tabela_memoria, num)
+        VALUES ('UPDATE', NEW.id, NEW.num);
     ELSIF TG_OP = 'DELETE' THEN
-        INSERT INTO log (operacao, id_tabela_memoria, num, id_transacao)
-        VALUES ('DELETE', OLD.id, OLD.num, id_transacao_atual);
+        INSERT INTO log (operacao, id_tabela_memoria, num)
+        VALUES ('DELETE', OLD.id, OLD.num);
     END IF;
 
     RETURN NEW;
